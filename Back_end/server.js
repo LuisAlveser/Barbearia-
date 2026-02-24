9f3a6c0cc03433b1236c74ae40a6cd94cefb6f05
LuisAlveser <ericssonluis@gmail.com>
2026-02-24 17:27:06 -0300

    feat : Criação do Front End

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,5 +1,7 @@
 import app from'./app.js'
 const porta=3001
+const cors = require('cors');
+app.use(cors());
 app.listen(porta,()=>{
     console.log("Servidor ligado")
 });
---
+++ Front_end/front_end/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Logo from "../src/assets/Logo.png"
+import './App.css'
+function Formulario(){
+  return(
+    <>
+    <div className='Formulario'>
+      
+    </div>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+     <div className='Fundo'>
+      <img src={Logo} className='Logo' />
+     </div>
+    </>
+  )
+}
+
+export default App
